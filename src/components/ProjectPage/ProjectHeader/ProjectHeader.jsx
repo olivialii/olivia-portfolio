9cabf5267b95521fca393e502e0cc22e47756a31
olivial215 <li.ol@northeastern.edu>
2025-01-28 20:40:35 -0500

    assets + project page header

--- src/components/ProjectPage/ProjectHeader/ProjectHeader.jsx
+++ src/components/ProjectPage/ProjectHeader/ProjectHeader.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import styles from "./ProjectHeader.module.css";
+import { Link } from 'react-router-dom';
 
 
 const ProjectHeader = () => {
+
     return (
       <div>
-        <div className={styles.ProjectTitle}>
-            <div> - Work / </div> 
-            <h2> SearchNEU </h2>
+        <div className={styles.header}>
+            <div className={styles.ProjectTitle}>
+                <Link to={"/Projects"}> - Work / </Link> 
+                <h2> SearchNEU </h2>
+            </div>
+            <p className={styles.desc}> SearchNEU is a comprehensive tool that helps Northeastern students search for course information, simplifying the course registration process. </p>
         </div>
 
-        <h1> SearchNEU</h1>
-        <p> desc </p>
-        <img></img>
+        <img className={styles.banner} src='/assets/searchBanner.png'></img>
+        
       </div>
     );
   };
