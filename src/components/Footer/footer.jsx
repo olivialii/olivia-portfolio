0b452af01a9d9efe84710850ce41944fe6c744e2
olivial215 <li.ol@northeastern.edu>
2025-02-04 03:29:36 -0500

    social links

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,14 +3,20 @@ import styles from "./footer.module.css";
 
 export const Footer = () => {
     return (
-        <div className={styles.Footer}> 
-            <div className={styles.Name}> <a href="/">olivia li</a></div>
-            <u1 className={styles.Socials}>
-                
-                <li><a href="https://www.linkedin.com/in/olivia-lii" target="_blank">LINKEDIN</a></li>
-                <li><a href="#email">EMAIL</a></li>
-                <li><a href="#resume">RESUME</a></li>
-            </u1>
+        <div>
+            <div className={styles.Footer}> 
+                <div className={styles.Name}> <a href="/">olivia li</a></div>
+                <u1 className={styles.Socials}>
+                    
+                    <li><a href="https://www.linkedin.com/in/olivia-lii" target="_blank">LINKEDIN</a></li>
+                    <li><a href="mailto:li.ol@northeastern.edu">EMAIL</a></li>
+                    <li><a href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:cced040a-6815-45d0-a80d-e1e04f90ff6d" target="_blank">RESUME</a></li>
+                </u1>
+            </div>
+
+            <div className={styles.bottom}>
+                Made w/ javascript, css, & react.
+            </div>
         </div>
     );
 };
