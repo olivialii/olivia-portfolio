697c614245258dc12d8bcdc96f0fffc14877eba6
olivial215 <li.ol@northeastern.edu>
2025-02-18 01:22:07 -0500

    mbta page

--- src/components/ProjectPage/mbta.jsx
+++ src/components/ProjectPage/mbta.jsx
@@ -36,15 +36,17 @@ const Mbta = () => {
         </div>}>
       </Subheader>
 
-      <div className={styles.construction}>
-          🚧 This page is under construction! 🚧
-      </div> <br/>
+
       
       <Banner 
         banner='/assets/mbta/banner.png'
       />
       
-
+      <Subheader
+        subheader= "Overview"
+        desc= {<div> This case study was completed as part of my Design Process + Practices course's "Project: Intervention – Interactive", where we learned how to use the design process to fully document and realize an interactive project. My task was to create an interactive tool that improves the experience on Boston's public transportation system. </div>}
+      />
+    
       <Details
         role="UI/UX Designer & Researcher"
         duration="February 2024 - March 2024"
@@ -53,6 +55,14 @@ const Mbta = () => {
       }
       />
 
+     
+        <div className="p-{50px}">
+        <iframe width={"100%"} height={"500px"} src="https://embed.figma.com/deck/bDclkeQAIZNt7V8TgyR308/MBTA-slides?node-id=2-117&viewport=-18%2C106%2C0.24&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&embed-host=share&footer=false" allowfullscreen></iframe>
+        </div>
+   
+      <br/><br/><br/>
+
+       {/** 
       <Subheader
         subheader= "Sitemap"
         desc= {
@@ -75,7 +85,7 @@ const Mbta = () => {
         </div>
         }
       />
-      
+      */}
       
     
 
